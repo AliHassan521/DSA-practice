74c04c0eb4fa4fc22d1b6967fad27a5816177032
Ali Hassan <alihassan057689234@gmail.com>
2024-08-26 23:26:21 +0500

    vectors in stl

--- vectors.cpp
+++ vectors.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print()
+{
+
+    // emplace_back() is faster than push_back()
+
+    // vector<int> v;
+    // v.push_back(1);
+    // v.emplace_back(2);
+
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+
+    // vector<pair<int, int>> v;
+    // v.push_back({1, 2});
+    // v.emplace_back(3, 4);
+
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i].first << " " << v[i].second << endl;
+    // }
+
+    // vector<int> v(5, 10); // 5 elements with value 10
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+
+    // vector<int> v1(5); // 5 elements with value 0
+    // for (int i = 0; i < v1.size(); i++)
+    // {
+    //     cout << v1[i] << " ";
+    // }
+}
+
 int main()
 {
+    print();
 }
