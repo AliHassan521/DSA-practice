1eb1a2dcdaca3504f52fa99d07c37f10b424cf4e
Ali Hassan <alihassan057689234@gmail.com>
2024-08-30 19:54:19 +0500

    sum of digits

--- math.cpp
+++ math.cpp
@@ -4,16 +4,26 @@ using namespace std;
 
 int main()
 {
-    // reverxe a number
-    int n;
+    int n, sum = 0;
     cin >> n;
     int temp = n;
-    int reverse = 0;
     while (temp > 0)
     {
         int lastDigit = temp % 10;
-        reverse = reverse * 10 + lastDigit;
+        sum += lastDigit;
         temp = temp / 10;
     }
-    cout << reverse << endl;
+    cout << sum << endl;
+    // reverse a number
+    // int n;
+    // cin >> n;
+    // int temp = n;
+    // int reverse = 0;
+    // while (temp > 0)
+    // {
+    //     int lastDigit = temp % 10;
+    //     reverse = reverse * 10 + lastDigit;
+    //     temp = temp / 10;
+    // }
+    // cout << reverse << endl;
 }
