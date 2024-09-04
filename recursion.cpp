c5b5180b57768e38796b060efe5879dade5eac02
Ali Hassan <alihassan057689234@gmail.com>
2024-09-04 14:49:16 +0500

    counter

--- recursion.cpp
+++ recursion.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cnt = 0;
+void fn()
+{
+    if (cnt == 4)
+        return;
+    cout << cnt << " ";
+    cnt++;
+    fn();
+}
+
 int main()
 {
     // a function that calls itself is called when a certain condition is met
+    fn();
 }
