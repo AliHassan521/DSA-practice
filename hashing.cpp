73ea4f2dd8950e49b1776830fa1f64333ced2ac7
Ali Hassan <alihassan057689234@gmail.com>
2024-09-08 16:03:58 +0500

    hashing in strings

--- hashing.cpp
+++ hashing.cpp
@@ -4,28 +4,47 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
+    // int n;
+    // cin >> n;
+    // int arr[n];
 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+    // // precomputation
+    // int hash[12] = {0};
+    // for (int i = 0; i < n; i++)
+    // {
+    //     hash[arr[i]]++;
+    // }
+
+    // int q;
+    // cin >> q;
+    // while (q--)
+    // {
+    //     int num;
+    //     cin >> num;
+    //     // fetching the value from the hash table
+    //     cout << hash[num] << endl;
+    // }
+
+    string s;
+    cin >> s;
+
     // precomputation
-    int hash[12] = {0};
-    for (int i = 0; i < n; i++)
+    int hash[26] = {0};
+    for (int i = 0; i < s.length(); i++)
     {
-        hash[arr[i]]++;
+        hash[s[i] - 'a']++;
     }
-
     int q;
     cin >> q;
     while (q--)
     {
-        int num;
-        cin >> num;
+        char c;
+        cin >> c;
         // fetching the value from the hash table
-        cout << hash[num] << endl;
+        cout << hash[c - 'a'] << endl;
     }
 }
